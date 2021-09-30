26c3a4291f3b7ecc6a623a921f5ace1b74b6d861
IsraelSenpai <raelfaustino9@gmail.com>
2021-09-30 19:17:20 -0300

    atualizacao

--- js/main.js
+++ js/main.js
@@ -1,6 +1,5 @@
 jQuery(document).ready(function( $ ) {
 
-  // Back to top button
   $(window).scroll(function() {
     if ($(this).scrollTop() > 100) {
       $('.back-to-top').fadeIn('slow');
@@ -13,7 +12,6 @@ jQuery(document).ready(function( $ ) {
     return false;
   });
 
-  // Header fixed on scroll
   $(window).scroll(function() {
     if ($(this).scrollTop() > 100) {
       $('#header').addClass('header-scrolled');
@@ -26,15 +24,12 @@ jQuery(document).ready(function( $ ) {
     $('#header').addClass('header-scrolled');
   }
 
-  // Real view height for mobile devices
   if (window.matchMedia("(max-width: 767px)").matches) {
     $('#intro').css({ height: $(window).height() });
   }
 
-  // Initiate the wowjs animation library
   new WOW().init();
 
-  // Initialize Venobox
   $('.venobox').venobox({
     bgcolor: '',
     overlayColor: 'rgba(6, 12, 34, 0.85)',
@@ -42,7 +37,6 @@ jQuery(document).ready(function( $ ) {
     closeColor: '#fff'
   });
 
-  // Initiate superfish on nav menu
   $('.nav-menu').superfish({
     animation: {
       opacity: 'show'
@@ -50,7 +44,6 @@ jQuery(document).ready(function( $ ) {
     speed: 400
   });
 
-  // Mobile Navigation
   if ($('#nav-menu-container').length) {
     var $mobile_nav = $('#nav-menu-container').clone().prop({
       id: 'mobile-nav'
@@ -90,7 +83,6 @@ jQuery(document).ready(function( $ ) {
     $("#mobile-nav, #mobile-nav-toggle").hide();
   }
 
-  // Smooth scroll for the menu and links with .scrollto classes
   $('.nav-menu a, #mobile-nav a, .scrollto').on('click', function() {
     if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
       var target = $(this.hash);
@@ -124,7 +116,6 @@ jQuery(document).ready(function( $ ) {
     }
   });
 
-  // Gallery carousel (uses the Owl Carousel library)
   $(".gallery-carousel").owlCarousel({
     autoplay: true,
     dots: true,
@@ -134,14 +125,11 @@ jQuery(document).ready(function( $ ) {
     }
   });
 
-  // Buy tickets select the ticket type on click
   $('#buy-ticket-modal').on('show.bs.modal', function (event) {
     var button = $(event.relatedTarget);
     var ticketType = button.data('ticket-type');
     var modal = $(this);
     modal.find('#ticket-type').val(ticketType);
   })
-
-// custom code
 
 });
